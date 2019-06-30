e4075ea9653ff445eb4b6528861c233e7c6af059
trupu <strubilowicz@zsp5kozuchow.pl>
2019-06-30 09:51:54 +0200

    Banner - Design // Animations

--- client/src/components/sites/Banner.vue
+++ client/src/components/sites/Banner.vue
@@ -2,13 +2,37 @@
     div.container
         nav.nav-menu
             div.nav-menu_logo
-                img(src='../../assets/logo.png')
-                span.nav-menu_logo__title
-                    | powerbeck
-            div.nav-menu_menu 
+                a.nav-menu_logo(href='/')
+                    img(src='../../assets/logo.png')
+                    span.nav-menu_logo__title
+                        | powerbeck
+            div.nav-menu_menu(@click='showMenu()' ref='menuButton')
                 span
                     | menu
                 i(class='fas fa-bars')
+            div.nav-menu_links(ref='myMenu')
+                div.nav-menu_links__exit(@click='hideMenu()' ref='exitButton')
+                    i(class='fas fa-times')
+                ul(class='menu-list')
+                    a(href='/')
+                        li
+                            | start
+                    a(href='#coaches')
+                        li
+                            | trenerzy
+                    a(href='#club')
+                        li
+                            | klub
+                    a(href='#')
+                        li
+                            | wideo
+                    a(href='#sauna')
+                        li
+                            | sauna
+                    a(href='#offer')
+                        li
+                            | oferta
+
 
         section.banner-main
             div.banner-main_wrapper
@@ -25,15 +49,84 @@
                 
 </template>
 <script>
-    import Buttons from "../pieces/Buttons";
+import Buttons from "../pieces/Buttons";
 
 export default {
     name:'Banner',
+    methods: {
+        showMenu(){
+            const menu = this.$refs.myMenu;
+            const on = 'menu-animation-on';
+            const off = 'menu-animation-off';
+
+            const li = document.querySelectorAll('.menu-list li');
+
+            let time = 0.5;
+
+            li.forEach((el,index) => {
+                time+=0.2;
+
+                el.classList.add('menu-li-anim');
+                el.style.animationDuration = `${time}s`;
+            });
+
+            if(menu.classList.contains(off)){
+                menu.classList.remove(off);
+            }
+            this.$refs.menuButton.style.opacity='0';
+            menu.classList.add(on);
+        },
+        hideMenu(){
+            const menu = this.$refs.myMenu;
+            const on = 'menu-animation-on';
+            const off = 'menu-animation-off';
+
+            const li = document.querySelectorAll('.menu-list li');
+
+            li.forEach((el,index) => {
+                el.classList.remove('menu-li-anim');
+            });
+
+            this.$refs.menuButton.style.opacity='1';
+
+            menu.classList.remove(on);
+            menu.classList.add(off);
+        }
+    }
 }
 </script>
 <style lang='scss' scoped>
     
     $default_site_color: #9e0012;
+
+    @keyframes menu-anim{
+        from{
+            transform: translateY(0);
+        }
+        to{
+            transform: translateY(500px);
+        }
+    }
+
+    @keyframes menu-anim-off{
+        from{
+            transform: translateY(500px);
+        }
+        to{
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes menu-li{
+        from{
+            transform: translateX(-50px);
+            opacity: 0;
+        }
+        to{
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
 
     .container{
         width:100vw;
@@ -49,6 +142,8 @@ export default {
     .nav-menu{
         display: flex;
         position: fixed;
+
+        z-index: 140;
 
         top: 0;
         left: 0;
@@ -69,7 +164,10 @@ export default {
             display: flex;
             align-items: center;
 
+            user-select: none;
             flex-flow: row;
+
+            text-decoration: none;
 
             img{
                 max-height: 50px;
@@ -77,7 +175,15 @@ export default {
             }
 
             span{
+                color: #fff;
+                text-shadow: 2px 2px 8px #000;
                 font-size: 1.2em;
+
+                transition: all .3s ease-in-out;
+
+                &:hover{
+                    color: $default_site_color;
+                }
             }
         }
 
@@ -85,14 +191,106 @@ export default {
             display: flex;
             align-items: center;
             flex-flow: row;
+            
+            user-select: none;
+            transition: all .3s ease-in-out;
 
             span{
+                text-shadow: 2px 2px 8px #000;
                 font-size: 1.2em;
                 margin: 0 3px;
             }
 
             i{
                 font-size: 1.3em;
+            }
+
+            &:hover{
+                cursor: pointer;
+                color: $default_site_color;
+            }
+        }
+        .nav-menu_links{
+            display: flex;
+            position: absolute;
+            top: -500px;
+            left: 0;
+
+            z-index: 150;
+
+            padding: 50px 0;
+
+            width: 100vw;
+            height: auto;
+
+            background-color: rgba(0,0,0, .8);
+
+            align-items: center;
+            justify-content: center;
+
+            .nav-menu_links__exit{
+                position: absolute;
+                top: 15px;
+                right: 30px;
+                font-size: 1.5em;
+
+                color: #fff;
+
+                transition: all .3s ease-in-out;
+
+                &:hover{
+                    cursor: pointer;
+                    color: $default_site_color;
+                }
+            }
+
+            ul{
+
+                width: 100%;
+
+                list-style-type: none;
+                padding: 0;
+                margin: 0;
+
+                a{
+                    font-size: 2em;
+                    text-decoration: none;
+
+
+                    li{
+                        line-height: 1.5;
+                        text-align: center;
+                        color: #fff;
+
+                        width: 100%;
+                        height: auto;
+
+                        transition: all .3s ease-in-out;
+
+                        &:hover{
+                            color: $default_site_color;
+                            background-color: rgba(255,255,255, .1);
+
+                            transform: translateX(-5px);
+                        }
+
+                        &::after{
+                            content: '\00bb';
+                            position: absolute;
+                            margin: 0 10px;
+
+                            opacity: 0;
+
+                            transition: margin .3s ease-in-out, opacity .3s ease-in-out;
+            
+                        }
+
+                        &:hover::after{
+                            opacity: 1;
+                            margin: 0 5px;
+                        }
+                    }
+                }
             }
         }
     }
@@ -143,7 +341,7 @@ export default {
                 font-size: 2.5em;
                 color: #fff;
                 
-                width: 100px;
+                width: 120px;
 
                 text-align: center;
 
@@ -158,7 +356,43 @@ export default {
                     cursor: pointer;
                     color: lighten($default_site_color, 30%);
                 }
+
+                &::after{
+                    content: 'Czytaj Dalej';
+                    font-size: 16px;
+                    font-weight: 300;
+                    text-transform: uppercase;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    text-align: center;
+                    opacity: 0;
+
+                    transition: opacity .3s ease-in-out, transform .3s ease-in-out;
+                }
+
+                &:hover::after{
+                    opacity:1;
+                    transform: translateY(-15px);
+                }
             }
         }
     }
+
+    @media (max-width:768px){
+    
+        .menu-li-anim{
+            animation: menu-li both;
+        }
+
+        .menu-animation-on{
+            animation: menu-anim .7s forwards;
+        }
+
+        .menu-animation-off{
+            animation: menu-anim-off .7s forwards;
+        }
+    }
+
 </style>
