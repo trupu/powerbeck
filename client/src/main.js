3f3581d93ebef69898b0108fc01524bef2183b1b
trupu <strubilowicz@zsp5kozuchow.pl>
2019-07-18 16:11:19 +0200

    Trying to use client-side routing

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
+import inConstruction from './inConstruction.vue';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/', component: App,
+  },
+  {
+      path: '/login', component: inConstruction,
+  },
+  {
+      path: '/contact', component: inConstruction,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+/* eslint-disable-next-line */
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
-}).$mount('#app');
+});
